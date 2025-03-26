29451854f7dd1169c8e9cf8d1745fd01ad956763
crazeoni <40152649+crazeoni@users.noreply.github.com>
2025-03-26 20:31:23 +0100

    api route fixes

--- trips/urls.py
+++ trips/urls.py
@@ -6,16 +6,9 @@ from .views import get_route, get_trip_logs
 router = DefaultRouter()
 router.register(r'trips', TripViewSet)
 
-# urlpatterns = [
-	# path('', home, name='home'),
-    # path('api/', include(router.urls)),
-    # path('api/get_route/', get_route, name="get_route"),
-    # path("api/trip-logs/<int:trip_id>/", get_trip_logs, name="trip_logs"),
-# ]
-# urlpatterns += router.urls  # ✅ Ensures all API endpoints are included
 urlpatterns = [
-    path("", home, name="home"),
-    path("", include(router.urls)),  # ✅ Removed "api/"
-    path("get_route/", get_route, name="get_route"),  # ✅ No need for "api/"
-    path("trip-logs/<int:trip_id>/", get_trip_logs, name="trip_logs"),  # ✅ No need for "api/"
+	path('', home, name='home'),
+    path('api/', include(router.urls)),
+    path('api/get_route/', get_route, name="get_route"),
+    path("api/trip-logs/<int:trip_id>/", get_trip_logs, name="trip_logs"),
 ]
